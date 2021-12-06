30c9a259002389f0aadfc6f7afca4575eadb4d66
bed72 <gjramos100@gmail.com>
2021-12-06 13:04:09 -0300

    :fire: Criando DI do modulo de Network e interceptor.

--- app/src/main/java/github/bed72/bedapp/framework/di/NetworkModule.kt
+++ app/src/main/java/github/bed72/bedapp/framework/di/NetworkModule.kt
@@ -5,18 +5,22 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import github.bed72.bedapp.BuildConfig
+import github.bed72.core.data.network.interceptor.AuthorizationInterceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.*
 import java.util.concurrent.TimeUnit
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
 
+    private const val TIMEOUT_SECONDS = 15L
+
     @Provides
-    fun provideLoggingInterceptor() : HttpLoggingInterceptor =
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor =
         HttpLoggingInterceptor().apply {
             setLevel(
                 if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY
@@ -25,23 +29,33 @@ object NetworkModule {
         }
 
     @Provides
+    fun provideAuthorizationInterceptor(): AuthorizationInterceptor =
+        AuthorizationInterceptor(
+            publicKey = BuildConfig.PUBLIC_KEY,
+            privateKey = BuildConfig.PRIVATE_KEY,
+            calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+        )
+
+    @Provides
     fun provideOkHttpClient(
-        loggingInterceptor: HttpLoggingInterceptor
-    ) : OkHttpClient = OkHttpClient.Builder()
+        loggingInterceptor: HttpLoggingInterceptor,
+        authorizationInterceptor: AuthorizationInterceptor
+    ): OkHttpClient = OkHttpClient.Builder()
         .addInterceptor(loggingInterceptor)
-        .readTimeout(15, TimeUnit.SECONDS)
-        .connectTimeout(15, TimeUnit.SECONDS)
+        .addInterceptor(authorizationInterceptor)
+        .readTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
+        .connectTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
         .build()
 
     @Provides
-    fun provideGsonConverterFactory() : GsonConverterFactory =
+    fun provideGsonConverterFactory(): GsonConverterFactory =
         GsonConverterFactory.create()
 
     @Provides
     fun provideRetrofit(
         okHttpClient: OkHttpClient,
         converterFactory: GsonConverterFactory
-    ) : Retrofit = Retrofit.Builder()
+    ): Retrofit = Retrofit.Builder()
         .baseUrl(BuildConfig.BASE_URL)
         .client(okHttpClient)
         .addConverterFactory(converterFactory)
