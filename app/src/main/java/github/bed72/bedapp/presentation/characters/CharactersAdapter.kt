4e5c478f46cb6b2ad734674445e2eb562a61d9fd
bed72 <gjramos100@gmail.com>
2021-12-06 14:00:18 -0300

    :fire: Criado o CharactersAdapter

---
+++ app/src/main/java/github/bed72/bedapp/presentation/characters/CharactersAdapter.kt
@@ -0,0 +1,30 @@
+package github.bed72.bedapp.presentation.characters
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import github.bed72.core.domain.entities.Character
+
+class CharactersAdapter : ListAdapter<Character, CharactersViewHolder>(diffCallback) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        CharactersViewHolder.create(parent)
+
+    override fun onBindViewHolder(holder: CharactersViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    companion object {
+        private val diffCallback = object : DiffUtil.ItemCallback<Character>() {
+            override fun areItemsTheSame(
+                oldItem: Character,
+                newItem: Character
+            ) = oldItem.name == newItem.name
+
+            override fun areContentsTheSame(
+                oldItem: Character,
+                newItem: Character
+            ) = oldItem == newItem
+        }
+    }
+}
