34b9fa74fa63f454c60580ec933cedb18fafbddd
Gabriel Ramos <login+bed72@elo7.com>
2022-03-07 14:02:54 -0300

    :tada: Remove implementação base para Adapters do Nested RecyclerView error no Lint

--- app/src/main/java/github/bed72/bedapp/presentation/detail/adapters/DetailChildAdapter.kt
+++ app/src/main/java/github/bed72/bedapp/presentation/detail/adapters/DetailChildAdapter.kt
@@ -1,6 +1,7 @@
 package github.bed72.bedapp.presentation.detail.adapters
 
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import github.bed72.bedapp.framework.imageloader.usecase.ImageLoader
 import github.bed72.bedapp.presentation.detail.entities.DetailChildViewEntity
 import github.bed72.bedapp.presentation.detail.viewholders.DetailChildViewHolder
@@ -8,11 +9,13 @@ import github.bed72.bedapp.presentation.detail.viewholders.DetailChildViewHolder
 class DetailChildAdapter(
     private val imageLoader: ImageLoader,
     private val detailChildList: List<DetailChildViewEntity>
-) : DetailNestedBaseAdapter<DetailChildViewEntity, DetailChildViewHolder>(detailChildList) {
+) : RecyclerView.Adapter<DetailChildViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
         DetailChildViewHolder.create(parent, imageLoader)
 
     override fun onBindViewHolder(holder: DetailChildViewHolder, position: Int) {
         holder.bind(detailChildList[position])
     }
+
+    override fun getItemCount() = detailChildList.size
 }
--- app/src/main/java/github/bed72/bedapp/presentation/detail/adapters/DetailParentAdapter.kt
+++ app/src/main/java/github/bed72/bedapp/presentation/detail/adapters/DetailParentAdapter.kt
@@ -1,6 +1,7 @@
 package github.bed72.bedapp.presentation.detail.adapters
 
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import github.bed72.bedapp.framework.imageloader.usecase.ImageLoader
 import github.bed72.bedapp.presentation.detail.entities.DetailParentViewEntity
 import github.bed72.bedapp.presentation.detail.viewholders.DetailParentViewHolder
@@ -8,13 +9,15 @@ import github.bed72.bedapp.presentation.detail.viewholders.DetailParentViewHolde
 class DetailParentAdapter(
     private val imageLoader: ImageLoader,
     private val detailParentList: List<DetailParentViewEntity>
-) : DetailNestedBaseAdapter<DetailParentViewEntity, DetailParentViewHolder>(detailParentList) {
+) : RecyclerView.Adapter<DetailParentViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
         DetailParentViewHolder.create(parent, imageLoader)
 
     override fun onBindViewHolder(holder: DetailParentViewHolder, position: Int) {
         holder.bind(detailParentList[position])
     }
+
+    override fun getItemCount() = detailParentList.size
 }
 
 
