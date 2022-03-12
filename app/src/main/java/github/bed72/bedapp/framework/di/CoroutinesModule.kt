56594b57bd8c9c196a1071d0fb828b10c941538f
Gabriel Ramos <login+bed72@elo7.com>
2022-03-12 13:33:26 -0300

    :tada: Implementa teste da ViewModel de Detalhes dos personagens.

--- app/src/main/java/github/bed72/bedapp/framework/di/CoroutinesModule.kt
+++ app/src/main/java/github/bed72/bedapp/framework/di/CoroutinesModule.kt
@@ -1,16 +1,15 @@
 package github.bed72.bedapp.framework.di
 
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
-import kotlinx.coroutines.Dispatchers
 import dagger.hilt.components.SingletonComponent
-import github.bed72.core.usecase.base.AppCoroutinesDispatchers
 import github.bed72.core.usecase.base.CoroutinesDispatchers
+import github.bed72.core.usecase.base.AppCoroutinesDispatchers
 
 @Module
 @InstallIn(SingletonComponent::class)
 interface CoroutinesModule {
-    @Provides
+    @Binds
     fun bindDispatchers(appCoroutinesDispatchers: AppCoroutinesDispatchers): CoroutinesDispatchers
 }
---
+++ testing/src/main/java/github/bed72/testing/model/ComicFactory.kt
@@ -0,0 +1,17 @@
+package github.bed72.testing.model
+
+import github.bed72.core.domain.model.Comic
+import github.bed72.testing.model.ComicFactory.FakeComic.FakeComicOne
+
+class ComicFactory {
+    fun create(comic: FakeComic) = when (comic) {
+        FakeComicOne -> Comic(
+            2211506,
+            "https://fakecomicurl.jpg"
+        )
+    }
+
+    sealed class FakeComic {
+        object  FakeComicOne : FakeComic()
+    }
+}
